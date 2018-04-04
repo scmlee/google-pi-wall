6891f27054c0c288716e6180967ff1fb173310c5
Simon Lee <scmlee@gmail.com>
2018-04-03 22:44:16 -0400

    Updates to the PIR code

--- js/motion-sensor.js
+++ js/motion-sensor.js
@@ -3,50 +3,74 @@
     var Raspi = require("raspi-io");
 
     var board = new five.Board({
+		repl: false,
+		sigint: false,
         io: new Raspi()
     });
 
     var SCREENOFF_DELAY = 30; //Seconds
+    
+    var turnOffScreen = function () {
+	}
+	
+	var turnOnScreen = function() {
+	}
+    
 
     board.on("ready", function () {
-        var l = new five.LCD({
-            controller: "PCF8574AT",
-            address: 0x38
-        });
+		try {
+			var l = new five.LCD({
+				controller: "PCF8574AT",
+				address: 0x38
+			});
 
-        var led = new five.Led("GPIO18");
-        var motion = new five.Motion("GPIO17");
-        var date = new Date();
-        var turned_off = false;
-        var last_motion_time = date.now();
+			var led = new five.Led("GPIO18");
+			var motion = new five.Motion("GPIO17");
+			var turned_off = false;
+			var last_motion_time = Date.now();
 
-        motion.on("calibrated", function () {
-            console.log("calibrated");
-            l.clear();
-            l.cursor(0, 0).print("PIR Calibrated");
-        })
+			l.cursor(0,0).print("Working...");
 
-        motion.on("motionstart", function () {
-            console.log("motionstart");
-            led.on();
+			motion.on("calibrated", function () {
+				console.log("calibrated");
+				l.clear();
+				l.cursor(0, 0).print("PIR Calibrated");
+			})
 
-            last_motion_time = date.now();
+			motion.on("motionstart", function () {
+				last_motion_time = Date.now();
+				
+				console.log(Date.now() + " motionstart");
+				l.clear();
+				l.cursor(0,0).print("motionstart");
+				l.cursor(1,0).print(last_motion_time);
+				led.on();
 
-            if (turned_off) {
-                console.log("Time to turn on the screen");
-                turned_off = false;
-            }
-        });
 
-        motion.on("motionend", function () {
-            console.log("motionend");
-            led.off();
+				if (turned_off) {
+					console.log("Time to turn on the screen");
+					turned_off = false;
+				}
+			});
 
-            if (!turned_off && date.now() > (last_motion_time + (SCREENOFF_DELAY * 1000))) {
-                console.log("time to turn off the screen");
-                turned_off = true;
-            }
-        });
+			motion.on("motionend", function () {
+				console.log(Date.now() + " motionend (lastmotiontime=" + last_motion_time + ")");
+				
+				l.clear();
+				l.cursor(0,0).print("motionend");
+				l.cursor(1,0).print(last_motion_time);
+				
+				led.off();
+
+				if (!turned_off && Date.now() > (last_motion_time + (SCREENOFF_DELAY * 1000))) {
+					console.log("time to turn off the screen");
+					turned_off = true;
+				}
+			});
+		} catch (error) {
+			console.log("Something went wrong...");
+			console.log(error);
+		}
     });
 
 })();
