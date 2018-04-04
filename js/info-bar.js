2b35f7894ded018e7c3aed67ca2c2cb1704e743c
Simon Lee <scmlee@gmail.com>
2018-04-03 21:49:42 -0400

    Tweak formatting

--- js/info-bar.js
+++ js/info-bar.js
@@ -12,7 +12,7 @@
     var request = require('request');
 
     function updateDateTime() {
-        document.querySelector("#datetime #date").innerText = moment().format("dddd, MMMM D YYYY");
+        document.querySelector("#datetime #date").innerText = moment().format("dddd, MMMM D");
         document.querySelector("#datetime #time").innerText = moment().format("h:mm A");
     }
 
