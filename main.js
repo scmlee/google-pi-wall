4713c07213db021f03930663288ecdbc3397e41f
Simon Lee <scmlee@gmail.com>
2018-04-11 22:02:27 -0400

    tweak calendar ui

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const electron = require('electron')
+const logger = require('electron-timber');
+
 // Module to control application life.
 const app = electron.app
-// Module to create native browser window.
+    // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
@@ -10,7 +12,7 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-// let backgroundWindow;
+    // let backgroundWindow;
 
 // function createBackgroundWindow() {
 // 	const win = new BrowserWindow({
@@ -22,35 +24,35 @@ let mainWindow
 // 	return win;
 // }
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    //kiosk: true
-  })
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        //kiosk: true
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  //Maximize the window
- // mainWindow.setFullScreen(true);
-  mainWindow.maximize();
+    //Maximize the window
+    // mainWindow.setFullScreen(true);
+    mainWindow.maximize();
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools(); //{ mode : "undocked"});
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools(); //{ mode : "undocked"});
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
 // This method will be called when Electron has finished
@@ -59,21 +61,21 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-    //backgroundWindow = createBackgroundWindow();
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+        //backgroundWindow = createBackgroundWindow();
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
